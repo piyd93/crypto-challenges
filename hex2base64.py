f41a5b0fb73745dd941cd5d23a9ae75274ef34e7
naomi- <naomi.l.touchet@gmail.com>
2017-04-05 22:37:51 -0700

    Starting challenge set one!

---
+++ hex2base64.py
@@ -0,0 +1,10 @@
+import base64
+import binascii
+
+hexstring = input('Enter the hex number to base64 encode.')
+
+binarystring = binascii.unhexlify(hexstring)
+
+print binascii.b2a_base64(binarystring)
+
+
